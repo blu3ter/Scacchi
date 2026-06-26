0df5d1b7a0c0ad540ecb7ef12053a54ae3ece400
Simone <simone.sc.catenaccio@gmail.com>
2026-06-26 02:27:13 +0200

    aggiornate tutte le foto sulla scacchiera

--- src/main/java/Controller/MenuController.java
+++ src/main/java/Controller/MenuController.java
@@ -7,33 +7,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+
 import javafx.stage.Stage;
 
 public class MenuController {
 
-      @FXML
+    @FXML
     private Button BtnGioca;
-    
 
- @FXML
-private void AvviaPartita() {
-    try {
-        // 1. Carica il nuovo file FXML della scacchiera
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Scacchiera.fxml"));
-        Parent root = loader.load();
 
-        // 2. Ottieni lo Stage attuale (la finestra) dal bottone cliccato
-        Stage stage = (Stage) BtnGioca.getScene().getWindow();
 
-        // 3. Cambia la scena con quella della scacchiera
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    } catch (IOException e) {
-        e.printStackTrace();
+    @FXML
+    private void AvviaPartita() {
+        try {
+            Stage stage = (Stage) BtnGioca.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Scacchiera.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
 
     @FXML
     private void Esci() {
